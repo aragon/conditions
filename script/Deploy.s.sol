80a984d6a42932ae19b6384f7b1c1ae3a4ae2240
Jør∂¡ <4456749+brickpop@users.noreply.github.com>
2025-09-15 15:18:17 +0200

    Leaving the single deployment internal deploys

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.22;
 
 import {Script, console} from "forge-std/Script.sol";
 import {ConditionFactory} from "../src/factory/ConditionFactory.sol";
+import {ExecuteSelectorCondition} from "../src/ExecuteSelectorCondition.sol";
+import {SelectorCondition} from "../src/SelectorCondition.sol";
+import {IDAO} from "@aragon/osx-commons-contracts/src/dao/IDAO.sol";
+import {IOwnerManager} from "../src/SafeOwnerCondition.sol";
+import {SafeMock} from "../test/mocks/SafeMock.sol";
 
 contract Deploy is Script {
     modifier broadcast() {
@@ -20,6 +25,20 @@ contract Deploy is Script {
         console.log("");
 
         ConditionFactory factory = new ConditionFactory();
+
+        // Deploy dummy instances to force verifying the source
+        ExecuteSelectorCondition.SelectorTarget[] memory initialEntries =
+            new ExecuteSelectorCondition.SelectorTarget[](0);
+        bytes4[] memory selectors = new bytes4[](0);
+
+        factory.deployExecuteSelectorCondition(IDAO(address(0)), initialEntries);
+        factory.deploySelectorCondition(IDAO(address(0)), selectors);
+
+        address safeAddress = vm.envOr("SAFE_ADDRESS", address(0));
+        if (safeAddress == address(0)) {
+            safeAddress = address(new SafeMock());
+        }
+        factory.deploySafeOwnerCondition(IDAO(address(0)), safeAddress);
 
         // Result
         console.log("Condition Factory:", address(factory));
